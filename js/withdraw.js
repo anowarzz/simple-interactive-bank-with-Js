1344019f203f8269f788d877eb999d6c5b248a03
anowarzz <anowarhosen444@gmail.com>
2022-08-17 18:45:34 +0600

    added some validation

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,14 +1,25 @@
 // Step 1. add event handler with the withdraw button.
-document.getElementById("btn-withdraw").addEventListener("click", function () {
-  // Step 2: get the withdraw amount from the withdraw input field.
-  // step 2.5: also make sure to convert the input into a number by using parse float.
+// Step 2: get the withdraw amount from the withdraw input field.
+// step 2.5: also make sure to convert the input into a number by using parse float.
+// step 3: get the previous withdraw total
+// Step 4: calculate total withdraw amount
+// step 5: get the previous balance total
+// step 6: calculate new balance total
+// set the new balance total
+// step 7: clear the input field
 
+document.getElementById("btn-withdraw").addEventListener("click", function () {
   const withdrawField = document.getElementById("withdraw-field");
 
   const newWithdrawAmountString = withdrawField.value;
   const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
-  // step 3: get the previous withdraw total
+  withdrawField.value = "";
+
+  if (isNaN(newWithdrawAmount)) {
+    alert("Please provide a number");
+    return;
+  }
 
   const withdrawTotalElement = document.getElementById("withdraw-total");
 
@@ -16,21 +27,18 @@ document.getElementById("btn-withdraw").addEventListener("click", function () {
 
   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-  // Step 4: calculate total withdraw amount
-
-  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  withdrawTotalElement.innerText = currentWithdrawTotal;
-
-  // step 5: get the previous balance total
   const balanceTotalElement = document.getElementById("balance-total");
   const previousBalanceTotalString = balanceTotalElement.innerText;
   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
-  // step 6: calculate new balance total
-  // set the new balance total
+  if (newWithdrawAmount > previousBalanceTotal) {
+    alert("Baap er bank e etoo taka nai 😒");
+    return;
+  }
+
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  withdrawTotalElement.innerText = currentWithdrawTotal;
+
   const newbalanceTotal = previousBalanceTotal - newWithdrawAmount;
   balanceTotalElement.innerText = newbalanceTotal;
-
-  // step 7: clear the input field
-  withdrawField.value = "";
 });
